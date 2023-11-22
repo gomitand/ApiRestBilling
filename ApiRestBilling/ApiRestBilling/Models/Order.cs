6f6a40096fdc360ca174414d9ceae79e6723040d
gomitand <gomitand@gmail.com>
2023-11-22 02:53:51 -0500

    feat(conf): add validatios to medel class

---
+++ ApiRestBilling/ApiRestBilling/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiRestBilling.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        [MaxLength(128)]
+
+        public Guid OrderNumber { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; } = 0;
+
+        [ForeignKey("CustomerId")]
+        public  Customer Customer { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+         
+
+          
+
+    }
+}
